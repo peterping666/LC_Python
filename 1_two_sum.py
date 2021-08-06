5ec044640060573a2ec409f33d16c143195a170b
Peter <peter.ping666@gmail.com>
2021-08-06 12:24:54 -0700

    first commit

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return []
         dic = {}
-        for i in range(len(nums)):
-            num = nums[i]
+        for i, num in enumerate(nums):
             if num in dic:
                 return (dic[num], i)
             else:
                 dic[target - num] = i
-        return ()
+        return []
