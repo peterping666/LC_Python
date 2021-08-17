8a1c45856af72e5d47b7f15cfdaed5f9ef6bf4af
Peter <peter.ping666@gmail.com>
2021-08-17 10:50:35 -0700

    review lc with python

---
+++ 215_Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,22 @@
+import heapq
+
+
+class Solution1:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if(len(heap) > k):
+                heapq.heappop(heap)
+        return heap[0]
+
+
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            elif num > heap[0]:
+                heapq.heapreplace(heap, num)
+        return heap[0]
